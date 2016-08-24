5aee2a37a96fcd172ab183a23c66bde6c926295d
Manoj Vas <manojvaseekharan.chromebook@gmail.com>
2016-08-24 10:41:45 -0400

    first commit of main feature

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	while(1)
+	{
+		//output prompt
+		//read line
+		//if line does not have ctrl+d character, continue, otherwise break
+		//tokenize
+		//process directives
+		//run command
+	}
+
+
+}
